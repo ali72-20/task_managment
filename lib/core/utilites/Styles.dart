7729b3c39bb3e6057bda1013864cbc3f1ba37581
ali72-20 <as20201118@gmail.com>
2024-09-18 01:49:55 +0300

    add text style 35 add start screen text

--- lib/core/utilites/Styles.dart
+++ lib/core/utilites/Styles.dart
@@ -1,3 +1,12 @@
-class Styles{
 
+
+import 'package:flutter/material.dart';
+import 'package:task_managment/core/utilites/colors.dart';
+
+class Styles{
+   static const TextStyle style35 =  TextStyle(
+      fontSize: 35,
+      fontFamily: kPrimaryFont,
+      fontWeight: FontWeight.w600
+   );
 }
--- lib/features/start_screen/presentation_layer/widgets/start_screen_body.dart
+++ lib/features/start_screen/presentation_layer/widgets/start_screen_body.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:task_managment/core/utilites/assets.dart';
 
+import '../../../../core/utilites/Styles.dart';
+
 class StartScreenBody extends StatelessWidget {
   const StartScreenBody({super.key});
 
@@ -10,11 +12,15 @@ class StartScreenBody extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16,vertical: 9),
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 9),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Image.asset(start_image),
+              const Text(
+                "Manage Your Life Here!",
+                style: Styles.style35,
+              )
             ],
           ),
         ),
